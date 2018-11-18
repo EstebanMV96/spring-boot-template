f2dbe18b0851ebeac8d9879eabc3b4ae778dc1d9
Esteban Moya <moyaco41@gmail.com>
2018-11-18 11:21:52 -0500

    add cors and request filter

--- src/main/java/com/manglu/template/TemplateApplication.java
+++ src/main/java/com/manglu/template/TemplateApplication.java
@@ -1,9 +1,21 @@
 package com.manglu.template;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = "com.manglu.template")
+@EnableAutoConfiguration
+@EntityScan(basePackages = "com.manglu.template")
+@EnableJpaRepositories("com.manglu.template.repository")
+@EnableScheduling
 public class TemplateApplication {
 
 	public static void main(String[] args) {
